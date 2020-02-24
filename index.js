d2c14047579bf39678d52670677df12815a661cb
James Pickard <james.pickard@gmail.com>
2020-02-24 11:37:11 +0000

    Add 1 second debounce to Slack post

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const conf = require("./.env.js");
 const Tail = require("tail-file");
 const { IncomingWebhook } = require("@slack/webhook");
+const debounce = require('debounce');
 
 const slack = new IncomingWebhook(conf.slackWebhookUrl);
+
+const buffers = {};
 
 const getFileConfigValue = (file, key) => {
   if (file[key] === undefined) {
@@ -12,30 +15,43 @@ const getFileConfigValue = (file, key) => {
   return file[key];
 };
 
+const sendBufferToSlack = file => {
+  const message = buffers[file.filepath].join("\n");
+
+  slack.send({
+    text: "```" + message + "```"
+  });
+
+  console.log(message);
+};
+
 for (const file of conf.filesToWatch) {
   console.log(`Started watching for changes in ${file.filepath}.`);
+
+  buffers[file.filepath] = [];
+
   new Tail(file.filepath, line => {
-    if (getFileConfigValue(file, "logPath")) {
-      line = `<${file.filepath}> ${line}`;
+    if (buffers[file.filepath].length === 0) {
+      // Only add metadata to the start of the buffer.
+      if (getFileConfigValue(file, "logPath")) {
+        line = `<${file.filepath}> ${line}`;
+      }
+
+      const metaData = [];
+      if (file.prefix) {
+        metaData.push(file.prefix);
+      }
+
+      if (getFileConfigValue(file, "logTimestamp")) {
+        metaData.push(new Date().toLocaleString());
+      }
+
+      if (metaData.length) {
+        line = `[${metaData.join(" ")}] ${line}`;
+      }
     }
 
-    const metaData = [];
-    if (file.prefix) {
-      metaData.push(file.prefix);
-    }
-
-    if (getFileConfigValue(file, "logTimestamp")) {
-      metaData.push(new Date().toLocaleString());
-    }
-
-    if (metaData.length) {
-      line = `[${metaData.join(" ")}] ${line}`;
-    }
-
-    slack.send({
-      text: line
-    });
-
-    console.log(line);
+    buffers[file.filepath].push(line);
+    debounce(sendBufferToSlack.bind(this, file), 1000)();
   });
 }
