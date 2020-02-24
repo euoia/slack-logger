e3c25350e3357a81b23ee0f874b3a473a08742d5
James Pickard <james.pickard@gmail.com>
2020-02-24 12:30:41 +0000

    Finally fix the use of debounce

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const getFileConfigValue = (file, key) => {
 };
 
 function sendBufferToSlack() {
+  console.log(`sendBufferToSlack`);
   const message = buffers[this.filepath].join("\n");
 
   slack.send({
@@ -30,7 +31,7 @@ for (const file of conf.filesToWatch) {
   console.log(`Started watching for changes in ${file.filepath}.`);
 
   buffers[file.filepath] = [];
-  file.sendBufferToSlack = sendBufferToSlack.bind(file);
+  file.sendBufferToSlack = debounce(sendBufferToSlack.bind(file), 1000);
 
   new Tail(file.filepath, line => {
     if (buffers[file.filepath].length === 0) {
@@ -54,6 +55,6 @@ for (const file of conf.filesToWatch) {
     }
 
     buffers[file.filepath].push(line);
-    debounce(file.sendBufferToSlack, 1000)();
+    file.sendBufferToSlack();
   });
 }
